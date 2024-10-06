e67785b32427d20eeae200c15d666ae45908a4dc
anu-raj-16 <anaghag.1612@gmail.com>
2024-10-05 17:33:40 -0700

    Added specification for CarRental class

---
+++ src/main/model/CarRental.java
@@ -0,0 +1,66 @@
+package model;
+
+//represents a Car Rental
+public class CarRental {
+
+    // EFFECTS: constructs a Car Rental with five default cars
+    public CarRental() {
+
+    }
+
+    // REQUIRES: car must not already be owned by this Car Rental
+    // MODIFIES: this
+    // EFFECTS: adds the given car to the list of cars in this
+    public void addCar(Car c) {
+
+    }
+
+    // REQUIRES: car has to be owned by this Car Rental
+    // MODIFIES: this
+    // EFFECTS: remove the given car from the list of cars in this
+    public void removeCar(Car c) {
+
+    }
+
+    // REQUIRES: a car has been returned just before calling this method and method
+    // should not be called again until another car has been returned or
+    // the same car was rented out and returned again.
+    // MODIFIES: this
+    // EFFECTS: calculates the total revenue made by adding
+    // the amount made by each car in the rental
+    public void calculateTotalRevenue() {
+
+    }
+
+    // REQUIRES: a valid car name that belongs to the car rental agency
+    // MODIFIES: this
+    // EFFECTS: rents out the car with the given name
+    public void rentACar(String name) {
+
+    }
+
+    // REQUIRES: a valid car name that belongs to the car rental agency
+    // MODIFIES: this
+    // EFFECTS: returns the car with the given name
+    // and updates the total revenue made
+    public void returnACar(String name) {
+
+    }
+
+    // EFFECTS: lists all the cars in car rental agency
+    public void listAllCars() {
+
+    }
+
+    // EFFECTS: lists all the cars in the car rental agency 
+    // available for renting
+    public void listAllCarsAvailable() {
+
+    }
+
+    // EFFECTS: lists all the cars in the car rental agency 
+    // that have been rented out
+    public void listAllCarsRentedOut() {
+
+    }
+}
