ec20d9ed287aad895c008a47e9067a1b5e5006cd
anu-raj-16 <anaghag.1612@gmail.com>
2024-10-07 20:16:41 -0700

    Added specification for Agency class in ui package

---
+++ src/main/ui/Agency.java
@@ -0,0 +1,89 @@
+package ui;
+
+// Car rental ageny app
+public class Agency {
+    // EFFECTS: runs the Car rental agency app
+    public Agency() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: starts the app, processes user information and 
+    // checks if app should keep running
+    public void runAgency() {
+        // stub
+    }
+
+    // EFFECTS: processes user input and performs the corresponding action
+    public void processInput() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes car rental agency
+    public void init() {
+         // stub
+    }
+
+    //EFFECTS: displays the options available to the user
+    public void displayMenu() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: rents out the car requested by user if user is eligible
+    public void rentOutACar() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: returns the car rented out by user
+    public void returnRentedCar() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a car to collection owned by the agency
+    public void addACar() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: remove a car from collection owned by the agency
+    public void removeACar() {
+        // stub
+    }
+
+    // EFFECTS: displays information of all cars owned by the car rental
+    // in the order - number, make, bodystyle, model
+    public void viewAllCars() {
+        // stub
+    }
+
+    // EFFECTS: displays information of all cars owned by the car rental that are available
+    // for renting in the order - number, make, bodystyle, model
+    public void viewAllAvailableCars() {
+        // stub
+    }
+
+    // EFFECTS: displays information of all cars owned by the car rental that have been
+    // rented out in the order - number, make, bodystyle, model
+    public void viewAllRentedCars() {
+        // stub
+    }
+
+    // EFFECTS: displays total revenue made by the agency
+    public void checkTotalRenvenue() {
+
+    }
+
+    // EFFECTS: displays the number of cars in the agancy
+    public void numberOfCars() {
+        // stub
+    }
+
+    // EFFECTS: displays the number of cars rented out in the agancy
+    public void numberOfRentedCars() {
+        // stub
+    }
+}
