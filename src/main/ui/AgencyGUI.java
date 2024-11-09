796d5aaa0db9cfa3999e848ea2ecd48a04f5b918
anu-raj-16 <anaghag.1612@gmail.com>
2024-11-09 09:35:42 -0800

    Added specification for AgencyGUI class

---
+++ src/main/ui/AgencyGUI.java
@@ -0,0 +1,29 @@
+package ui;
+
+public class AgencyGUI {
+
+    // EFFECTS: constructs the main window and initializes the Car Rental system
+    public AgencyGUI() {
+
+    }
+
+    // EFFECTS: initializes the Car Rental system
+    public void initialize() {
+
+    }
+
+    // EFFECTS: adds a car to the system
+    public void addCar() {
+
+    }
+
+    // EFFECTS: saves car rental to file 
+    public void save() {
+
+    }
+
+    // EFFECTS: loads car rental from file
+    public void load() {
+        
+    }
+}
