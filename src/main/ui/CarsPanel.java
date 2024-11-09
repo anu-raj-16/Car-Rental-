bb3008ac1d1db82efebfbb849c38958e367c71e0
anu-raj-16 <anaghag.1612@gmail.com>
2024-11-09 10:55:11 -0800

    Added specifications for CarsPanel class

---
+++ src/main/ui/CarsPanel.java
@@ -0,0 +1,19 @@
+// Reference: https://github.students.cs.ubc.ca/CPSC210/B02-SpaceInvadersBase
+package ui;
+
+import javax.swing.JPanel;
+
+import model.CarRental;
+
+public class CarsPanel extends JPanel {
+
+    // EFFECTS: contructs a panel with all cars in the car rental
+    public CarsPanel(CarRental cr) {
+
+    }
+
+    // EFFECTS: updates the panel of all cars
+    public void update() {
+
+    }
+}
